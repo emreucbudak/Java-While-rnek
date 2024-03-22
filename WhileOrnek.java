3bb100666bad22b980735faa51d39016872f3acc
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-03-22 14:06:50 +0300

    Javada While Örneği
    Java While Örneği

---
+++ WhileOrnek.java
@@ -0,0 +1,15 @@
+package test;
+
+public class WhileOrnek {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while (i<10) {
+			System.out.println("İ nin değeri =  " +i);
+			i++;
+		}
+
+	}
+
+}
